a60036c718864ec89644d0c68c88d0d7b309cfad
Jacob Wong <jdw2@ualberta.ca>
2018-01-27 19:27:09 -0700

    Add icon iMage

--- app/src/main/java/com/example/jacob/weed_map/MapsActivity.java
+++ app/src/main/java/com/example/jacob/weed_map/MapsActivity.java
@@ -121,7 +121,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         currentLocationMarker = mMap.addMarker(markerOptions);
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(12));
 
         if (client != null){
             LocationServices.FusedLocationApi.removeLocationUpdates(client, this);
