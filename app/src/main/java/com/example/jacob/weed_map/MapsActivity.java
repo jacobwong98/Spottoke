fb4560ac099547cebf5e5edc1ff2986f1b0211ca
Melisse Doroteo <gabbie_doroteo@yahoo.com>
2018-01-27 19:25:19 -0700

    marker test

--- app/src/main/java/com/example/jacob/weed_map/MapsActivity.java
+++ app/src/main/java/com/example/jacob/weed_map/MapsActivity.java
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -38,9 +39,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        //Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").icon(BitmapDescriptorFactory.fromResource(R.drawable.poop)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+//        BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.poop.png);
+//        LatLng sydney = new LatLng(-33.852, 151.211);
+//        MarkerOptions markerOptions = new MarkerOptions().position(sydney)
+//                .title("Marker in Sydney")
+//                .snippet("snippet snippet snippet snippet snippet...")
+//                .icon(icon);
+//        mMap.addMarker(markerOptions);
+
+//        MarkerOptions options = new MarkerOptions()
+//                            .position(new LatLng(-33.852, 151.211))
+//                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+//                            .title("Sydney")
+//                            .snippet("here");
+//        marker = mGoogleMap.addMarker(options);
+
+//        renton = new MarkerOptions()
+//                .position(new LatLng(-33 151))
+//                .title("hi")
+//                .icon(BitmapDescriptorFactory.fromResource(
+//                        R.drawable.ic_launcher));
+//                )):
+
     }
 }
